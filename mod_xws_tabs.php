563b36dd77f639b4743e5e3f6c4d622866a7bb55
Papa Pathé Séne <pathe.sene@gmail.com>
2010-07-19 19:10:52 +0000

    bugfix   correction de la valeur par défaut du parametre style css  par défaut dans le manifeste xml

--- mod_xws_tabs.php
+++ mod_xws_tabs.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
  * Xws_Content Joomla Module
  * @package xws_content
  * @version 0.0.3
@@ -8,7 +8,7 @@
  * Copyright 2010, Papa Pathé Séne - Xarala Web Studios Sénégal
  * Licensed under GPL licence
  *
- * Date: sam. 17 juil. 2010 23:35:32 GMT 
+ * Date: sam. 17 juil. 2010 23:35:32 GMT
  *
  *--------------------------------------------------------------------------------------------- */
 
@@ -18,8 +18,8 @@
 
 /* Definition of XwsJoomla constants
  *------------------------------------------------------------------------ */
-  define('_XWS_CONTENT_PATH', 'modules'.DS.'mod_xws_tabs');  
-  define('_XWS_CONTENT_JS_PATH', _XWS_CONTENT_PATH.DS.'js'.DS);  
+  define('_XWS_CONTENT_PATH', 'modules'.DS.'mod_xws_tabs');
+  define('_XWS_CONTENT_JS_PATH', _XWS_CONTENT_PATH.DS.'js'.DS);
   define('_XWS_CONTENT_CSS_PATH', _XWS_CONTENT_PATH.DS.'css'.DS);
 
 /* Include the helper only once
@@ -34,45 +34,62 @@
 /* Get user desired template
  * ------------------------------------------------------------------------------------ */
     $moduleTemplate = $params->get('getTemplate','accordion');
-   
+
 /* Load javascript assets
  * load jquery library
  * load jquery-ui library
  * load xwsUiHelpers
  * ------------------------------------------------------------------------------------ */
   if ($params->get('load_jquery') == 1) {
-    JHTML::script('jquery-1.4.2.js', _XWS_CONTENT_JS_PATH, false);         
+    if ($params->get('environment') == 0) {
+      JHTML::script('jquery-1.4.2.js', _XWS_CONTENT_JS_PATH, false);
+    }
+    else {
+
+    }
   }
-  
+
   if ($params->get('load_jquery_ui') == 1) {
-    JHTML::script('jquery-ui.1.8.1.js', _XWS_CONTENT_JS_PATH, false);         
+    if ($params->get('environment') == 0) {
+      JHTML::script('jquery-ui.1.8.1.js', _XWS_CONTENT_JS_PATH, false);
+    }
+    else {
+
+    }
   }
-  
+
   if ($params->get('load_xws_helpers') == 1) {
-    JHTML::script('jquery.xws.uiHelpers.js', _XWS_CONTENT_JS_PATH, false);          
-  } 
-    
+    if ($params->get('environment') == 0) {
+      JHTML::script('jquery.xws.uiHelpers.js', _XWS_CONTENT_JS_PATH, false);
+    }
+    else {
+
+    }
+  }
+
 /* Load jquery ui css assets
  *
  * Set this option to no if you're using XwsJoomla Templates
  * ------------------------------------------------------------------------------------- */
   if ($params->get('load_jquery_ui_stylesheets') == 1)  {
-  
-    /* Set the user desired theme  
+
+    /* Set the user desired theme
      * Load the user desired theme
      * --------------------------------------------------------------------------------- */
-      $style = _XWS_CONTENT_CSS_PATH.DS.$params->get('getStyle').DS;    
-      JHTML::stylesheet('jquery-ui.css', $style, array());   
+      if ($params->get('environment') == 0) {
+        $style = _XWS_CONTENT_CSS_PATH.DS.$params->get('getStyle').DS;
+        JHTML::stylesheet('jquery-ui.css', $style, array());
+      }
 
-    /* Load the xws content 
-     * This css style contains overrides for jquery ui  
+    /* Load the xws content
+     * This css style contains overrides for jquery ui
      * -------------------------------------------------------------------------------- */
       JHTML::stylesheet('xws-content.css', _XWS_CONTENT_CSS_PATH, array());
-     
+
   }
 
-    
+
 /* Require the template
  * ------------------------------------------------------------------------------------- */
   require(JModuleHelper::getLayoutPath('mod_xws_tabs', $moduleTemplate.DS.'default'));
-  
+
