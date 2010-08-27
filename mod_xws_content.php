afa6ad657abcaaf38bb479440f5f458e459d5fa5
Papa Pathé Séne <pathe.sene@gmail.com>
2010-08-27 04:31:52 +0000

    refactoring plugin going for production in JED

--- mod_xws_content.php
+++ mod_xws_content.php
@@ -33,58 +33,11 @@
 
 /* Get user desired template
  * ------------------------------------------------------------------------------------------- */
-    $moduleTemplate = $params->get('getTemplate','accordion');
+    $moduleTemplate = $params->get('jquery-ui-template','accordion');
 
-/* Load javascript assets
- * Check for environment parameter to load files locally or by google ajax apis
- * load jquery library
- * load jquery-ui library
- * load xwsUiHelpers
- * ------------------------------------------------------------------------------------------- */
-  if ($params->get('load_jquery') == 1) {
-    if ($params->get('environment') == 0) {
-      JHTML::script('jquery-1.4.2.js', _XWS_CONTENT_JS_PATH, false);
-    }
-    else {
-        #TODO load_jquery from google ajax apis
-    }
-  }
 
-  if ($params->get('load_jquery_ui') == 1) {
-    if ($params->get('environment') == 0) {
-      JHTML::script('jquery-ui-1.8.2.js', _XWS_CONTENT_JS_PATH, false);
-    }
-    else {
-         #TODO load_jquery_ui from google ajax apis
-    }
-  }
 
-  if ($params->get('load_xws_helpers') == 1) {
-    JHTML::script('jquery.xws.uiHelpers.js', _XWS_CONTENT_JS_PATH, false);
-  }
 
-/* Load jquery ui css assets
- *
- * Check for environment parameter to load files locally or by google ajax apis
- * ------------------------------------------------------------------------------------- */
-  if ($params->get('load_jquery_ui_stylesheets') == 1)  {
-
-    /* Set the user desired theme
-     * Load the user desired theme
-     * --------------------------------------------------------------------------------- */
-      if ($params->get('environment') == 0) {
-        JHTML::stylesheet('jquery-ui.css', _XWS_CONTENT_CSS_PATH.DS.$params->get('getStyle').DS, array());
-      }
-      else {
-         #TODO load_jquery_ui_stylesheets from google ajax apis
-      }
-
-    /* Load the xws content
-     * This css style contains overrides for jquery ui
-     * -------------------------------------------------------------------------------- */
-      JHTML::stylesheet('xws-content.css', _XWS_CONTENT_CSS_PATH, array());
-
-  }
 
 
 /* Require the template
